59d2e296530d146243031f82a82d7e3c852f8ccb
andr <anderr225@gmail.com>
2018-05-26 16:14:06 +0700

    fix_task1

--- problems2/kamkin/task1/task1.py
+++ problems2/kamkin/task1/task1.py
@@ -4,14 +4,17 @@ def numberReader():
     while True:
         try:
             buf = sys.stdin.read(1)
-            int(buf)
+            num = int(buf)
+            print(num)
             return
         except EOFError:
+            print("EOFError", file=sys.stderr)
             return
         except ValueError:
+            print("ValueError", file=sys.stderr)
             continue
         except KeyboardInterrupt:
+            print("KeyboardInterrupt", file=sys.stderr)
             return
-
 
 numberReader()
